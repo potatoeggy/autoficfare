0dbc1516e64a87f653dce7189daef9c35d06eb5c
eggy <eggyrules@gmail.com>
2021-05-20 14:14:04 -0400

    Remove useless checks for plugin validation

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,12 +1,9 @@
-import abc
 import configparser
 
 class Plugin(object):
-    __metaclass__ = abc.ABCMeta
     def __init__(self, config: configparser.ConfigParser):
         self.config = config
     
 
-    @abc.abstractmethod
     def post_add_hook(self, metadata_list: list):
         return
